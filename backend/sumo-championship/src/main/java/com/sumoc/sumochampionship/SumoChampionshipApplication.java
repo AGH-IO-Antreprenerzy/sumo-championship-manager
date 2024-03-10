789fcd90e1a8502b4954cb9d67b9d6eb735f8401
Szymon Twardosz <szymont18@interia.pl>
2024-03-10 17:25:34 +0100

    Database configuration. Main project class with Test class

---
+++ backend/sumo-championship/src/main/java/com/sumoc/sumochampionship/SumoChampionshipApplication.java
@@ -0,0 +1,13 @@
+package com.sumoc.sumochampionship;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumoChampionshipApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SumoChampionshipApplication.class, args);
+	}
+
+}
---
+++ backend/sumo-championship/src/test/java/com/sumoc/sumochampionship/SumoChampionshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumoc.sumochampionship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumoChampionshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
