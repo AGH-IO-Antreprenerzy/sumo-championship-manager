94d7d8707dd5aceb16cf6211d56cb26bffd4455c
Arkadiusz Mincberger <410730@student.agh.edu.pl>
2024-03-18 21:07:35 +0100

    random fix

--- frontend/sumo-championship/src/components/Atoms/Submit.tsx
+++ frontend/sumo-championship/src/components/Atoms/Submit.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import "./../../styles/Atoms.css"
+
 interface props{
     label: string
 }
+
 const Submit: React.FC<props> = ({label}) => {
     return (
         <input type='submit' className='submit' value={label}/>
