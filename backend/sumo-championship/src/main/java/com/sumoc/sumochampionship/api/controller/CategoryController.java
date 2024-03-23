42f614fcb109894ff67a793847c3c2a68807f820
Szymon Twardosz <szymont181@gmail.com>
2024-03-23 14:35:33 +0100

    [AN-24] Fix Cors (#17)

--- backend/sumo-championship/src/main/java/com/sumoc/sumochampionship/api/controller/CategoryController.java
+++ backend/sumo-championship/src/main/java/com/sumoc/sumochampionship/api/controller/CategoryController.java
@@ -4,12 +4,10 @@ import com.sumoc.sumochampionship.api.dto.response.CategoriesResponse;
 import com.sumoc.sumochampionship.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/category")
 public class CategoryController {
--- backend/sumo-championship/src/main/java/com/sumoc/sumochampionship/api/controller/SeasonController.java
+++ backend/sumo-championship/src/main/java/com/sumoc/sumochampionship/api/controller/SeasonController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
  * Implements POST for adding new seasons (and their categories)
  */
 @RequiredArgsConstructor
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/season")
 public class SeasonController {
