21d295cd9ab5c5d6894abf56ed9cb92a3a6e5960
Michał Skałka <michal.skalka@swmansion.com>
2024-03-24 12:49:14 +0100

    Add new api methods

--- frontend/sumo-championship/src/api/api.ts
+++ frontend/sumo-championship/src/api/api.ts
@@ -1,26 +1,33 @@
+const DOMAIN = 'http://localhost:8080/api/';
+
+const throwFetchError = (
+  method: string,
+  endpoint: string,
+  response: Response,
+) => {
+  throw new Error(`[${method}] ${endpoint}: ${response.statusText}`);
+};
+
 function get(endpoint: string, params = {}) {
   return async () => {
-    try {
-      const uri =
-        'http://localhost:8080/api/v1/season/all?page=0&size=6&historical=false';
-      console.log(uri);
-      const response = await fetch(uri, {
-        method: 'Get',
-        headers: {
-          'Content-Type': 'application/json',
-          accept: 'application/json',
-        },
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        return data;
-      }
-
-      throw new Error('[GET] Error fetching data: ' + response.statusText);
-    } catch (error) {
-      console.error(error);
+    let uri = `${DOMAIN}${endpoint}`;
+    if (params) {
+      uri = `${uri}?${new URLSearchParams(params)}`;
     }
+
+    const response = await fetch(uri, {
+      method: 'Get',
+      headers: {
+        'Content-Type': 'application/json',
+        accept: 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    }
+    throwFetchError('GET', endpoint, response);
   };
 }
 
@@ -32,7 +39,64 @@ function getPaginated(endpoint: string, size: number, params = {}) {
   };
 }
 
+function post(endpoint: string, body = {}) {
+  return async () => {
+    const response = await fetch(`${DOMAIN}${endpoint}`, {
+      method: 'Post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (response.ok) {
+      const contentType = response.headers.get('content-type');
+      if (contentType && contentType.indexOf('application/json') !== -1) {
+        const data = await response.json();
+        return data;
+      }
+      return null;
+    }
+    throwFetchError('POST', endpoint, response);
+  };
+}
+
+function delete_(endpoint: string, body = {}) {
+  return async () => {
+    const response = await fetch(`${DOMAIN}${endpoint}`, {
+      method: 'Delete',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!response.ok) {
+      throwFetchError('DELETE', endpoint, response);
+    }
+  };
+}
+
+function put(endpoint: string, body = {}) {
+  return async () => {
+    const response = await fetch(`${DOMAIN}${endpoint}`, {
+      method: 'Put',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!response.ok) {
+      throwFetchError('PUT', endpoint, response);
+    }
+  };
+}
+
 export default {
   get,
   getPaginated,
+  post,
+  delete_,
+  put,
 };
