31dd4c11acb23f65eb88260e0e53287a6bf99e64
Michał Skałka <michal.skalka@swmansion.com>
2024-03-26 00:18:57 +0100

    Style login form

--- frontend/sumo-championship/src/pages/LoginPage.tsx
+++ frontend/sumo-championship/src/pages/LoginPage.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import LoginForm from '../components/organisms/LoginForm';
 import './../styles/Pages.css';
+import Tile from '../components/Atoms/Tile';
 const LoginPage: React.FC = () => {
   return (
     <div className="page loginPageLayout">
       <img src={require('./../assets/icons/sumo.png')} alt="sumo" />
-      <LoginForm />
+      <Tile>
+        <LoginForm />
+      </Tile>
       <img src={require('./../assets/icons/sumo.png')} alt="sumoIcon" />
     </div>
   );
