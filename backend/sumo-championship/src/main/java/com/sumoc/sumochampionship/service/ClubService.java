443a1be4fedd4bb21a8def7c5079319fed0579ea
Szymon Twardosz <szymont18@interia.pl>
2024-04-08 17:14:50 +0200

    [AN-121] Add Get for clubs that are available for user and wrestler

--- backend/sumo-championship/src/main/java/com/sumoc/sumochampionship/service/ClubService.java
+++ backend/sumo-championship/src/main/java/com/sumoc/sumochampionship/service/ClubService.java
@@ -1,17 +1,24 @@
 package com.sumoc.sumochampionship.service;
 
+import com.sumoc.sumochampionship.api.dto.club.ClubDto;
 import com.sumoc.sumochampionship.api.dto.club.ClubRequest;
 import com.sumoc.sumochampionship.db.people.Club;
+import com.sumoc.sumochampionship.db.people.WebsiteUser;
+import com.sumoc.sumochampionship.db.people.Wrestler;
 import com.sumoc.sumochampionship.repository.ClubRepository;
+import com.sumoc.sumochampionship.repository.WrestlerRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class ClubService {
     private final ClubRepository clubRepository;
+    private final WrestlerRepository wrestlerRepository;
 
     public String addClub(ClubRequest clubRequest){
         Optional<Club> clubOptional = clubRepository.findByName(clubRequest.getName());
@@ -23,4 +30,27 @@ public class ClubService {
         clubRepository.save(club);
         return "Club added successfully";
     }
+
+    /*
+    Get all Clubs that should be accessed by users
+     */
+    public List<ClubDto> getAllClubsToUser(WebsiteUser user){
+        return user.getOwnedClubs().stream().map(ClubDto::mapToDto).toList();
+    }
+
+    /*
+    Get Club to Wrestler
+     */
+    public ClubDto getClubToWrestler(Long wrestlerId){
+        Optional<Wrestler> wrestlerOptional = wrestlerRepository.findById(wrestlerId);
+
+        if (wrestlerOptional.isEmpty()){
+            throw new EntityNotFoundException("Model not found");
+        }
+
+        Wrestler wrestler = wrestlerOptional.get();
+
+        return ClubDto.mapToDto(wrestler.getClub());
+    }
+
 }
