66143cd9742e215bc268e77edfd24d597b8085fd
Arkadiusz Mincberger <410730@student.agh.edu.pl>
2024-04-12 23:14:50 +0200

    one more time

--- frontend/sumo-championship/src/components/organisms/AddTrainerForm/AddTrainerForm.tsx
+++ frontend/sumo-championship/src/components/organisms/AddTrainerForm/AddTrainerForm.tsx
@@ -6,6 +6,7 @@ import SelectField from '../../molecules/SelectField';
 import { getClubs } from '../../../api/club';
 import { addTrainer } from '../../../api/trainer';
 import { useNavigate } from 'react-router-dom';
+import ROUTES from '../../../routes/routes';
 
 const AddTrainerForm = () => {
     const navigate = useNavigate()
