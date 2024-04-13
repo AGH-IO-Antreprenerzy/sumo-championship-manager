bcacee438610ca5aaf498715f9a75699cda4978c
Arkadiusz Mincberger <410730@student.agh.edu.pl>
2024-04-13 23:28:32 +0200

    cahgens

--- frontend/sumo-championship/src/components/Atoms/WeightBox.tsx
+++ frontend/sumo-championship/src/components/Atoms/WeightBox.tsx
@@ -12,7 +12,7 @@ const WeightCategoryBox: React.FC<props> = ({value, onClick}) => {
     return (
         <div className='weightBox' style={{backgroundColor: value.isChoosen ? "#f37316" : "#a3a3a3"}}>
             <div> {`< ${value.maxWeight} kg`}</div>
-            <IconButton name={value.isChoosen ? 'cross' : 'plus'} size={16} onClick={onClick}/>
+            <IconButton name={value.isChoosen ? 'FaCross' : 'FaPlus'} size={16} onClick={onClick}/>
         </div>
     );
 };
